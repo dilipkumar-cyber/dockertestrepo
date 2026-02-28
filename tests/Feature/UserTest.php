ccf1eceebf0da6853c9de4395daa4cea0efb64c5
Dilip-Mobile-Dev <dilipkumarchepuri@kluniversity.in>
2026-02-28 10:14:21 +0530

    usertest

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\User;
+
+it('can create a user', function () {
+    User::factory()->create([
+        'name' => 'Alice',
+        'email' => 'alice@example.com',
+    ]);
+
+    // Use $this to access the method
+    $this->assertDatabaseHas('users', [
+        'email' => 'alice@example.com',
+    ]);
+});
